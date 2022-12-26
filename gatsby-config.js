4601163be620e10f94eb20449142347076ab532e
Juan Rodriguez <lx11472@innovaccion.mx>
2022-12-26 15:08:33 -0500

    feat: Assets and data added

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,34 @@
 module.exports = {
   siteMetadata: {
     title: `Juan Rodriguez`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-sass',
+    'gatsby-transformer-json',
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-smoothscroll`,
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/data/',
+      },
     },
-    __key: "images"
-  }]
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'assets',
+        path: './src/assets/',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: './src/assets/personal_logo.svg',
+      },
+    },
+  ],
 };
