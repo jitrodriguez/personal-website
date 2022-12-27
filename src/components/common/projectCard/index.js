1dce38ad9c85468ef0329e42e62b2a3920997f8e
Juan Rodriguez <lx11472@innovaccion.mx>
2022-12-26 21:33:19 -0500

    refactor: added link and repository

--- src/components/common/projectCard/index.js
+++ src/components/common/projectCard/index.js
@@ -9,11 +9,18 @@ export default function ProjectCard({ info }) {
       <div className='project-descriptor'>
         <h3>{info.Title}</h3>
         <p>{info.Stack}</p>
-        <a href={info.Repository}>
-          Repository
-          <LinkExternal />
-        </a>
-        {/* {info.Link && <a  />} */}
+        {info.Repository && (
+          <a href={info.Repository}>
+            Repository
+            <LinkExternal />
+          </a>
+        )}
+        {info.Link && (
+          <a href={info.Link}>
+            Link
+            <LinkExternal />
+          </a>
+        )}
       </div>
       <GatsbyImage
         style={{ zIndex: -1, width: '100%', height: '100%' }}
